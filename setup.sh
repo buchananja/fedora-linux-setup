9c24f67995c35b2489a0698368cc1812ee206449
James Buchanan <buchanan.j.a@pm.me>
2024-03-16 18:39:34 +0000

    Updated script to install vscode.

--- setup.sh
+++ setup.sh
@@ -1,24 +1,27 @@
 #! /bin/bash
 
-# creates directory for roboto fonts
-mkdir -p /usr/share/fonts/roboto
+# installs gnome tweaks
+sudo dnf install gnome-tweaks
 
-# downloads fonts from google fonts
-fonts=("Flex" "Slab" "Serif" "Mono")
-
-for font in "${fonts[@]}"; do 
-    wget -P /tmp "https://fonts.google.com/download?family=Roboto+${font}"
-    unzip "/tmp/download?family=Roboto+${font}" -d /usr/share/fonts/roboto
-done
+# installs adobe source fonts from fedora repository
+sudo dnf install adobe-source-code-pro-fonts adobe-source-sans-pro-fonts adobe-source-serif-pro-fonts
 
 # rebuilds font cache
 fc-cache -fv
 
+# sets terminal default font
+gsettings reset org.gnome.desktop.interface monospace-font-name
+
 # sets system default fonts
-gsettings set org.gnome.desktop.interface font-name 'Roboto Flex Regular 11'
-gsettings set org.gnome.desktop.interface document-font-name 'Roboto Serif Regular 11'
-gsettings set org.gnome.desktop.interface monospace-font-name 'Roboto Mono 11'
-gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Roboto Flex Bold 11'
+gsettings set org.gnome.desktop.interface font-name 'Source Sans 3 Regular 12'
+gsettings set org.gnome.desktop.interface document-font-name 'Source Serif 4 Regular 12'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Source Code Pro Regular 12'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Source Sans 3 Bold 12'
 
 # sets font scaling
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.2
+
+# install vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+printf "[vscode]\nname=packages.microsoft.com\nbaseurl=https://packages.microsoft.com/yumrepos/vscode/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscode.repo
+sudo dnf install code
