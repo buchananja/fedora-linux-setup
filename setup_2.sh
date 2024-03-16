35580776976891cf936e15b12528e4bf73f49d26
James Buchanan <buchanan.j.a@pm.me>
2024-03-16 17:28:17 +0000

    Fixed font naming.

--- setup_2.sh
+++ setup_2.sh
@@ -7,10 +7,10 @@ dnf install google-roboto-fonts google-roboto-mono-fonts
 fc-cache -fv
 
 # sets system default fonts
-gsettings set org.gnome.desktop.interface font-name 'Roboto Regular 11'
-gsettings set org.gnome.desktop.interface document-font-name 'Roboto Slab Regular 11'
-gsettings set org.gnome.desktop.interface monospace-font-name 'Roboto Mono 11'
-gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Roboto Bold 11'
+gsettings set org.gnome.desktop.interface font-name 'Roboto-Regular 11'
+gsettings set org.gnome.desktop.interface document-font-name 'Roboto-Regular 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Roboto-Mono 11'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Roboto-Regular 11'
 
 # sets font scaling
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.2
